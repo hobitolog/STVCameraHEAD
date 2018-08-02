da6f0dd29aeaab37f5d621bebf5380e3e1966067
fsmuggler <you@example.com>
2018-08-02 10:19:35 +0200

    function js update

--- web page/script.js
+++ web page/script.js
@@ -1,33 +1,20 @@
+speedU = "";
+speedD = "";
+
+angleU = "";
+angleD = "";
 
 
-
-direction = "";
-
-function moveLeft() {
+function SendSpeed(){
     nocache = "&nocache=" + Math.random() * 1000000;
     var request = new XMLHttpRequest();
-    direction = "&L1=L";
-    request.open("GET", "ajax_inputs" + direction + nocache, true);
+    request.open("GET", "ajax_inputs&L1=" + "U" + speedU + "D" + speedD + "E" + nocache, true);
     request.send(null);
 }
-function moveRight() {
+
+function SendAngle(){
     nocache = "&nocache=" + Math.random() * 1000000;
     var request = new XMLHttpRequest();
-    direction = "&L1=R";
-    request.open("GET", "ajax_inputs" + direction + nocache, true);
-    request.send(null);
-}
-function moveClockwise() {
-    nocache = "&nocache=" + Math.random() * 1000000;
-    var request = new XMLHttpRequest();
-    direction = "&L1=C";
-    request.open("GET", "ajax_inputs" + direction + nocache, true);
-    request.send(null);
-}
-function moveCounterClockwise() {
-    nocache = "&nocache=" + Math.random() * 1000000;
-    var request = new XMLHttpRequest();
-    direction = "&L1=A";
-    request.open("GET", "ajax_inputs" + direction + nocache, true);
+    request.open("GET", "ajax_inputs&L1=" + "U" + angleU + "D" + angleD + "E" + nocache, true);
     request.send(null);
 }
